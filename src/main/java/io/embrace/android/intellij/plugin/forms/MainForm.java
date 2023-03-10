f4685c979a112040906f69a2ab7ee69203ff0ce0
SolArabehety <sol.arabehety@gmail.com>
2023-03-10 15:10:21 -0300

    added button

--- src/main/java/io/embrace/android/intellij/plugin/forms/MainForm.java
+++ src/main/java/io/embrace/android/intellij/plugin/forms/MainForm.java
@@ -3,13 +3,26 @@ package io.embrace.android.intellij.plugin.forms;
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainForm {
     private JPanel container;
-    private JButton openDashboardButton;
+    private JButton connectToEmbraceButton;
 
     public MainForm(ToolWindow toolWindow) {
 
+
+        connectToEmbraceButton.addActionListener(e -> {
+            // define the URL to open in the browser
+            String url = "https://dash.embrace.io/";
+
+            // open the URL in the default browser
+            try {
+                Desktop.getDesktop().browse(new java.net.URI(url));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
     public JPanel getContent() {
