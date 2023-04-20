85e389fa842726db8479a7b7fcf8ec98b3c57b23
SolArabehety <sol.arabehety@gmail.com>
2023-04-20 13:23:27 -0300

    migrated ui components to kotlin

--- src/test/kotlin/io/embrace/android/intellij/plugin/MyPluginTest.kt
+++ src/test/kotlin/io/embrace/android/intellij/plugin/MyPluginTest.kt
@@ -29,12 +29,11 @@ class MyPluginTest : BasePlatformTestCase() {
         myFixture.testRename("foo.xml", "foo_after.xml", "a2")
     }
 
-//    TODO check if we need this test
-//    fun testProjectService() {
-//        val projectService = project.service<MyProjectService>()
-//
-//        assertEquals(4, projectService.getRandomNumber())
-//    }
+    fun testProjectService() {
+        val projectService = project.service<MyProjectService>()
+
+        assertEquals(4, projectService.getRandomNumber())
+    }
 
     override fun getTestDataPath() = "src/test/testData/rename"
 }
