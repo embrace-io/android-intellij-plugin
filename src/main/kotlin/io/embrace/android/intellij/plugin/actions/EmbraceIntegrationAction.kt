ac9c7cd750209390907f2fa31ef43961c8403d36
SolArabehety <sol.arabehety@gmail.com>
2023-04-20 14:55:34 -0300

    migrated action to kotlin

---
+++ src/main/kotlin/io/embrace/android/intellij/plugin/actions/EmbraceIntegrationAction.kt
@@ -0,0 +1,24 @@
+package io.embrace.android.intellij.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ToolWindow;
+import javax.swing.Icon
+
+
+internal class EmbraceIntegrationAction : AnAction {
+
+    constructor() : super()
+
+    constructor(text: String, description: String, icon: Icon) : super(text, description, icon)
+
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val toolWindowManager = event.project?.let { ToolWindowManager.getInstance(it) }
+        val window: ToolWindow? = toolWindowManager?.getToolWindow("Embrace Integration Assistant")
+        window?.show()
+    }
+
+
+}
