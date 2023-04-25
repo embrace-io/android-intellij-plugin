c0d2ffc3addcd13099b886c9bb7ff60e89a4d2f7
Lucas <lucaslabari@gmail.com>
2023-04-25 17:56:45 -0300

    Fix basePath issue.

--- src/main/kotlin/io/embrace/android/intellij/plugin/dataproviders/EmbraceIntegrationDataProvider.kt
+++ src/main/kotlin/io/embrace/android/intellij/plugin/dataproviders/EmbraceIntegrationDataProvider.kt
@@ -6,7 +6,11 @@ import java.io.IOException
 import java.io.PrintWriter
 
 
-internal class EmbraceIntegrationDataProvider(private val repo : EmbracePluginRepository) {
+internal class EmbraceIntegrationDataProvider(
+    private val repo: EmbracePluginRepository,
+    private val basePath: String?
+    ) {
+
     private val lastEmbraceVersion = repo.getLastSDKVersion()
 
     fun getSdkExampleCode(): String {
@@ -23,7 +27,7 @@ internal class EmbraceIntegrationDataProvider(private val repo : EmbracePluginRe
         return getResourceAsText("/examplecode/embrace_start.txt") ?: ""
     }
 
-    fun modifyGradleFile(basePath: String?) {
+    fun modifyGradleFile() {
         try {
             val file = File("$basePath/build.gradle")
             val sb = "classpath \"io.embrace:embrace-swazzler:5.14.0\""
@@ -36,13 +40,13 @@ internal class EmbraceIntegrationDataProvider(private val repo : EmbracePluginRe
         }
     }
 
-    internal fun createEmbraceFile(basePath: String?, appId: String, token: String) {
-        basePath?.let {
+    internal fun createEmbraceFile(appId: String, token: String) {
+        basePath?.let { path ->
             val configFile = getResourceAsText("/examplecode/config_template.txt") ?: ""
                 .replace("MY_APP_ID", appId)
                 .replace("MY_TOKEN", token)
 
-            repo.createEmbraceConfigFile(configFile, basePath)
+            repo.createEmbraceConfigFile(configFile, path)
         }
     }
 
--- src/main/kotlin/io/embrace/android/intellij/plugin/ui/forms/EmbraceIntegrationFactory.kt
+++ src/main/kotlin/io/embrace/android/intellij/plugin/ui/forms/EmbraceIntegrationFactory.kt
@@ -9,11 +9,11 @@ import io.embrace.android.intellij.plugin.repository.EmbracePluginRepository
 import io.embrace.android.intellij.plugin.repository.network.ApiService
 
 class EmbraceIntegrationFactory : ToolWindowFactory {
-    private val apiService = ApiService()
-    private val repo = EmbracePluginRepository(apiService)
-    private val dataProvider = EmbraceIntegrationDataProvider(repo)
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val apiService = ApiService()
+        val repo = EmbracePluginRepository(apiService)
+        val dataProvider = EmbraceIntegrationDataProvider(repo, project.basePath)
         val myToolWindow = EmbraceIntegrationForm(dataProvider)
         val contentFactory = ContentFactory.SERVICE.getInstance()
         val content = contentFactory.createContent(myToolWindow.getContent(), "", false)
