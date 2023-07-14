c38864d1663978db19e8cc268b7719c042e5ea87
SolArabehety <sol.arabehety@gmail.com>
2023-07-14 15:49:05 -0300

    updated gray text color

--- src/main/kotlin/io/embrace/android/intellij/plugin/ui/constants/Colors.kt
+++ src/main/kotlin/io/embrace/android/intellij/plugin/ui/constants/Colors.kt
@@ -5,7 +5,7 @@ import com.intellij.ui.JBColor
 internal object Colors {
     // regular - dark theme colors
     val grayBackground = JBColor(JBColor.decode("#d9d9d9"), JBColor.decode("#5c5c5c"))
-    val grayText = JBColor(JBColor.decode("#A9A9A9"), JBColor.decode("#C0C0C0"))
+    val grayText = JBColor(JBColor.decode("#A9A9A9"), JBColor.decode("#adadad"))
     val errorColor  = JBColor(JBColor.decode("#8B0000"), JBColor.decode("#FF6F6F"))
     val successColor  = JBColor(JBColor.decode("006400"), JBColor.decode("#90EE90"))
 
